09a95fe6b3ca0e5aa61839c57d96c64cb939a283
orange-na <otuy1125abman@gmail.com>
2024-11-10 19:15:46 +0900

    コード移行

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,41 @@
+import ProjectCard from "@/features/projectCard";
+import TopTitle from "@/features/topTitle";
+
 export default function Home() {
-  return <div />;
+  return (
+    <main className="container sm:px-16 m-auto">
+      <TopTitle />
+
+      <div className="pb-20 js-show-on-scroll">
+        <div className="container pt-40 mx-auto grid sm:grid-cols-2 gap-x-10 gap-y-[120px] px-5">
+          <ProjectCard
+            imgSrc="/yutonamba.png"
+            title="Mentorixer"
+            technologies={["Golang", "Next.js"]}
+            codeLink="https://github.com/orange-na/yutoprofile.git"
+            demoLink="https://yutoprofile.vercel.app/"
+          />
+          <ProjectCard
+            imgSrc="/yutonamba.png"
+            title="This is my portfolio site."
+            technologies={["Next.js"]}
+            codeLink="https://github.com/orange-na/yutoprofile.git"
+            demoLink="https://yutoprofile.vercel.app/"
+          />
+        </div>
+      </div>
+
+      <div className="fixed w-screen h-screen top-0 left-0 -z-20">
+        <div className="bg-gray-400 blur-3xl w-80 h-80 rounded-full absolute -z-10 animate-pulse" />
+        <div className="bg-gray-400 blur-3xl w-60 h-60 rounded-full absolute top-[450px] left-[1100px] -z-10 animate-pulse" />
+        <div className="bg-gray-400 blur-3xl w-40 h-40 rounded-full absolute top-[300px] left-[400px] -z-10 animate-pulse" />
+        <div className="bg-gray-400 blur-2xl w-24 h-24 rounded-full absolute top-[700px] left-[600px] -z-10 animate-pulse3" />
+        <div className="bg-gray-400 blur-3xl w-48 h-48 rounded-full absolute top-[100px] left-[1500px] -z-10 animate-pulse2" />
+        <div className="bg-gray-400 blur-3xl w-48 h-48 rounded-full absolute top-[500px] left-[0px] -z-10 animate-pulse" />
+        <div className="bg-gray-400 blur-3xl w-48 h-48 rounded-full absolute top-[900px] left-[200px] -z-10 animate-pulse3" />
+        <div className="bg-gray-400 blur-3xl w-48 h-48 rounded-full absolute top-[150px] left-[800px] -z-10 animate-pulse2" />
+        <div className="bg-gray-400 blur-3xl w-24 h-24 rounded-full absolute top-[50px] left-[1300px] -z-10 animate-pulse3" />
+      </div>
+    </main>
+  );
 }
---
+++ src/features/topTitle/index.tsx
@@ -0,0 +1,82 @@
+export default async function TopTitle() {
+  return (
+    <div className="flex flex-col h-[100vh] items-center justify-center js-show-on-scroll">
+      <div className="flex flex-col items-center">
+        <div className="flex gap-8 sm:gap-10">
+          <div className="flex">
+            <h1 className="text-[30px] tracking-[6px] leading-none animate-appear font-thin sm:tracking-[15px] hover:animate-bounce cursor-pointer sm:text-[55px]">
+              Y
+            </h1>
+            <h1 className="text-[30px] tracking-[6px] leading-none animate-appear font-thin sm:tracking-[15px] hover:animate-bounce cursor-pointer sm:text-[55px]">
+              U
+            </h1>
+            <h1 className="text-[30px] tracking-[6px] leading-none animate-appear font-thin sm:tracking-[15px] hover:animate-bounce cursor-pointer sm:text-[55px]">
+              T
+            </h1>
+            <h1 className="text-[30px] tracking-[6px] leading-none animate-appear font-thin sm:tracking-[15px] hover:animate-bounce cursor-pointer sm:text-[55px]">
+              O
+            </h1>
+          </div>
+          <div className="flex">
+            <h1 className="text-[30px] tracking-[6px] leading-none animate-appear font-thin sm:tracking-[15px] hover:animate-bounce cursor-pointer sm:text-[55px]">
+              N
+            </h1>
+            <h1 className="text-[30px] tracking-[6px] leading-none animate-appear font-thin sm:tracking-[15px] hover:animate-bounce cursor-pointer sm:text-[55px]">
+              A
+            </h1>
+            <h1 className="text-[30px] tracking-[6px] leading-none animate-appear font-thin sm:tracking-[15px] hover:animate-bounce cursor-pointer sm:text-[55px]">
+              M
+            </h1>
+            <h1 className="text-[30px] tracking-[6px] leading-none animate-appear font-thin sm:tracking-[15px] hover:animate-bounce cursor-pointer sm:text-[55px]">
+              B
+            </h1>
+            <h1 className="text-[30px] tracking-[6px] leading-none animate-appear font-thin sm:tracking-[15px] hover:animate-bounce cursor-pointer sm:text-[55px]">
+              A
+            </h1>
+          </div>
+        </div>
+        <p className="text-[13px] tracking-[6px] font-noto font-bold sm:text-[18px] sm:tracking-[15px] text-parimary">
+          This is my portfolio site.
+        </p>
+      </div>
+      <div className="flex flex-col -scale-y-100 items-center text-gray-950">
+        <div className="flex gap-8 sm:gap-10 italic">
+          <div className="flex">
+            <h1 className="text-[30px] tracking-[6px] leading-none animate-appear font-thin sm:tracking-[15px] hover:animate-bounce cursor-pointer sm:text-[55px]">
+              Y
+            </h1>
+            <h1 className="text-[30px] tracking-[6px] leading-none animate-appear font-thin sm:tracking-[15px] hover:animate-bounce cursor-pointer sm:text-[55px]">
+              U
+            </h1>
+            <h1 className="text-[30px] tracking-[6px] leading-none animate-appear font-thin sm:tracking-[15px] hover:animate-bounce cursor-pointer sm:text-[55px]">
+              T
+            </h1>
+            <h1 className="text-[30px] tracking-[6px] leading-none animate-appear font-thin sm:tracking-[15px] hover:animate-bounce cursor-pointer sm:text-[55px]">
+              O
+            </h1>
+          </div>
+          <div className="flex">
+            <h1 className="text-[30px] tracking-[6px] leading-none animate-appear font-thin sm:tracking-[15px] hover:animate-bounce cursor-pointer sm:text-[55px]">
+              N
+            </h1>
+            <h1 className="text-[30px] tracking-[6px] leading-none animate-appear font-thin sm:tracking-[15px] hover:animate-bounce cursor-pointer sm:text-[55px]">
+              A
+            </h1>
+            <h1 className="text-[30px] tracking-[6px] leading-none animate-appear font-thin sm:tracking-[15px] hover:animate-bounce cursor-pointer sm:text-[55px]">
+              M
+            </h1>
+            <h1 className="text-[30px] tracking-[6px] leading-none animate-appear font-thin sm:tracking-[15px] hover:animate-bounce cursor-pointer sm:text-[55px]">
+              B
+            </h1>
+            <h1 className="text-[30px] tracking-[6px] leading-none animate-appear font-thin sm:tracking-[15px] hover:animate-bounce cursor-pointer sm:text-[55px]">
+              A
+            </h1>
+          </div>
+        </div>
+        <p className="text-[13px] tracking-[6px] font-noto font-bold sm:text-[18px] leading-none sm:tracking-[15px] text-gray-300 italic">
+          This is my portfolio site.
+        </p>
+      </div>
+    </div>
+  );
+}
